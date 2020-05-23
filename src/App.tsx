a5c7c4e1a4a6161714c330f012a9e143c05a523b
six-two <info@six-two.dev>
2020-05-24 09:55:19 +1200

    Split CSS into more parts

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,9 @@ import { State } from './app/redux/store';
 
 
 // TODO: next steps
+// hide copy button on hexdump output
+// add hexdump settings
 //  - improve output line breaks (and use monospaced font)
-//      - add hexdump output
 //  - a menu to quickly jump to any section? like <a href="#export-link">Export link</a>
 // try like https://muzzybear.github.io/react-hexviewer/
 //  - explain that export is a snapshot, reword "session"
